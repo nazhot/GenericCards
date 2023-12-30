b1d0f67f2d07a7ad75b35a3aa2cc0f1d4a103ff7
nazhot <15216154+nazhot@users.noreply.github.com>
2023-12-30 09:17:02 -0700

    Adding to Game Header

--- src/game.h
+++ src/game.h
@@ -11,4 +11,16 @@ struct Game {
     uint8_t numDecks;
 };
 
+/**
+ * Initialize a new Game with default settings
+ *
+ * The first Player will be the default starter, Decks won't be set to anything
+ * in the beginning, that will be up to the programmer to initialize.
+ *
+ * @param numPlayers how many Players are in the Game
+ * @param numDecks how many Decks are in the Game
+ * @return pointer to the created Game
+ */
+struct Game* game_initialize( uint8_t numPlayers, uint8_t numDecks );
+
 #endif
