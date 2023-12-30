c845a2f73d4b402de7b6cc565306f947bc9f8a5d
nazhot <15216154+nazhot@users.noreply.github.com>
2023-12-30 07:18:53 -0700

    Starting Deck File
    Getting some initialize header file structs/enums/functions laid out.

---
+++ src/deck.h
@@ -0,0 +1,36 @@
+#ifndef DECK_H
+#define DECK_H
+#include <cstdint>
+
+enum EmptyDeckOption {
+    EDO_END_GAME,
+    EDO_SHUFFLE,
+    EDO_DEAL_HANDS
+};
+
+struct Deck {
+    unsigned int numCards;
+    struct Card *cards;
+    enum EmptyDeckOption whenEmpty; 
+};
+
+/**
+ * Initialize a deck, using an int to represent the cards within it
+ *
+ * Each Card is represented by a bit within the int. The ordering is Ace through
+ * King, and Diamonds, Clubs, Hearts, Spades.
+ *
+ * @param cards bits representing the Cards present in the deck
+ * @return pointer to the created Deck
+ */
+Deck* deck_initializeFromInt( uint64_t cards );
+
+/**
+ * Initialize a standard deck, with Ace through King, all Suits
+ *
+ * @return pointer to the created Deck
+ */
+Deck* deck_initializeStandard();
+
+
+#endif
