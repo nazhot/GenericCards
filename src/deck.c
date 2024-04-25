22443e8d3ad50bfe750822ea2c18a8ca83416007
nazhot <15216154+nazhot@users.noreply.github.com>
2024-04-24 18:21:09 -0600

    Make Standard Deck and Add Card Functions

--- src/deck.c
+++ src/deck.c
@@ -1,9 +1,7 @@
 #include "deck.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-
-
+#include <stdbool.h>
 
 Deck* deck_initializeEmpty( uint maxCards ) {
     Deck *deck = malloc( sizeof( Deck ) );
@@ -19,5 +17,24 @@ Deck* deck_initializeEmpty( uint maxCards ) {
     deck->maxCards = maxCards;
     deck->numCards = 0;
     deck->whenEmpty = 0;
+    return deck;
+}
+
+bool deck_addCard( Deck *deck, Card card ) {
+    if ( !deck ) return false;
+
+    if ( deck->numCards == deck->maxCards ) return false;
+
+    deck->cards[deck->numCards++] = card;
+    return true;
+}
+
+Deck* deck_initializeStandard() {
+    Deck *deck = deck_initializeEmpty( 52 );
+    for ( uint i = 0; i < 13; ++i ) {
+        for ( uint j = 0; j < 4; ++j ) {
+            deck_addCard( deck, ( Card ) { .value = i, .suit = j } );
+        }
+    }
     return deck;
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
+#include "deck.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 
 int main( int argc, char *argv[] ) {
+    deck_initializeStandard();
     return 0;
 }
